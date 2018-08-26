3386cc22c1cd4bdd882b79bd04369c66a5ea0239
venq <jevgenijs.vaikulis@gmail.com>
2018-08-26 13:56:05 +0300

    first one

---
+++ mono_test.php
@@ -0,0 +1,11 @@
+﻿<?php
+  
+  require __DIR__ . '/vendor/autoload.php';
+  
+  /*
+    monolog test
+    $log = new Monolog\Logger('name');
+    $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+    $log->addWarning('Foo');
+  */
+?>
